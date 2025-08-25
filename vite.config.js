d7821ef6944529604404f37772edb1cee456d3fd
vitorgabrielopalinhas <vitorgabriel14zuadas@gmail.com>
2025-08-25 17:15:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+    import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react( )],
-  base: '/', // Altere esta linha para barra simples
-})
+    // https://vitejs.dev/config/
+    export default defineConfig({
+      plugins: [react( )],
+      base: '/', // Mantenha esta linha como barra simples
+      build: {
+        outDir: 'dist', // Garante que a saída seja na pasta 'dist'
+        rollupOptions: {
+          input: {
+            main: 'index.html' // Garante que o ponto de entrada seja o index.html
+          }
+        }
+      }
+    })
